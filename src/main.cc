64059186f31f23293b956fc5a68556c9e4b4b444
Mike Loomis <Loomismeister@gmail.com>
2016-10-02 17:52:36 -0400

    Resetting build to helloworld...

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,7 @@
 #include <iostream>
-#include <QtGui/QApplication>
-#include <mdi.h>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello World\n";
-  QApplication app(argc, argv);
-   Mdi mainWindow;
-   mainWindow.show();
-
-   return app.exec();
+int main(int argc, char *argv[])
+{
+  std::cout << "Sup chris\r\n";
   return 0;
 }
